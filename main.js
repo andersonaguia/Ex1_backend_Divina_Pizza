bc46c1456f7f67551082891f33ffb33419b3bb5f
andersonaguiar <54728366+andersonaguia@users.noreply.github.com>
2022-10-07 20:04:00 -0300

    [M2S02] Ex 2 - Rotas de gerenciamento de pizzas

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 const express = require('express')
-
+const { v4: uuidv4 } = require('uuid')
 const app = express()
+app.use(express.json())
+
+let pizzas = []
+
+app.get('/pizzas', (request, response) => {
+    const nameQuery = request.query.name || ""
+
+    const pizzasFiltered = pizzas.filter(pizza => pizza.name.toLowerCase().includes(nameQuery.toLowerCase()))
+
+    response.json(pizzasFiltered)
+})
+
+app.post('/pizzas', (request, response) => {
+    const { name, description, price, url } = request.body
+
+    const pizzaExists = pizzas.find(pizza => pizza.name === name)
+
+    if(pizzaExists){
+        return response.status(401).json({error: "Já existe uma pizza com o mesmo nome!"})
+    }
+
+    const pizza = {
+        id: uuidv4(),
+        name: name,
+        description,
+        url,
+        price
+    }
+    pizzas.push(pizza)
+
+    response.status(201).json(pizzas)
+})
 
 app.listen(3333, () => {
     console.log("Server is running!")
